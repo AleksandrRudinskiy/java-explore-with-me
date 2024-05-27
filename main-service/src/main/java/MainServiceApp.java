621c28839bf223598c0bc4dcfcea783eefca717f
RatonLaveur <AjaxRid@yandex.ru>
2024-05-27 18:19:33 +0300

    stats#4

--- main-service/src/main/java/MainServiceApp.java
+++ main-service/src/main/java/MainServiceApp.java
@@ -1,11 +1,9 @@
-package ru.practicum;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class StatsClientApp {
+public class MainServiceApp {
+
     public static void main(String[] args) {
-        SpringApplication.run(StatsClientApp.class, args);
+        SpringApplication.run(MainServiceApp.class, args);
     }
 }
